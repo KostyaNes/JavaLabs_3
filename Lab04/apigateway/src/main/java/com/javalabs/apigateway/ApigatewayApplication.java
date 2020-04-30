680944512a4cf65db21a13a19fcf9a3d0b0b69d5
Kostya Nesterenko <31660955+KostyaNes@users.noreply.github.com>
2020-04-30 11:37:04 +0300

    Fixed Grafana

--- Lab04/apigateway/src/main/java/com/javalabs/apigateway/ApigatewayApplication.java
+++ Lab04/apigateway/src/main/java/com/javalabs/apigateway/ApigatewayApplication.java
@@ -2,12 +2,14 @@ package com.javalabs.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableRetry
+@EnableCircuitBreaker
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
