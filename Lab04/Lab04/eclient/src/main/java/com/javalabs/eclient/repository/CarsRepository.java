a2b744f8516e94abf90266e4e87e7ce5a2fda9e3
Kostya Nesterenko <2000kostia@gmail.com>
2020-04-30 11:31:44 +0300

    Revert "Fixed grafana"
    This reverts commit bec6fcac84ce4210c837b7cc68397915f3c4c8e7.

--- Lab04/Lab04/eclient/src/main/java/com/javalabs/eclient/repository/CarsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javalabs.eclient.repository;
-
-import com.javalabs.eclient.entity.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarsRepository extends CrudRepository<Car, Integer> {
-
-}
